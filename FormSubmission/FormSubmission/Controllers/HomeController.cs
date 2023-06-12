8f1e40ff44cc711cbbdd9f82c188d6096e0eb1ff
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2023-06-12 14:39:31 +0600

    Form Submission

---
+++ FormSubmission/FormSubmission/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using FormSubmission.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Login()
+        {
+            //ViewBag.Name = Request["Username"];
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string Username, string Password) {
+            //ViewBag.Name = form["Username"];
+            ViewBag.Name = Username;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult SignUp() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(User u) {
+            //var test = u;
+            if (ModelState.IsValid) { 
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+    }
+}
