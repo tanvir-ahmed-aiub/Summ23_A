24815c46e64ef72cff5c9012ad5d306d198a5197
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2023-07-23 15:25:43 +0600

    Intro Tier

--- IntroTierApp/BLL/Services/StudentService.cs
+++ IntroTierApp/BLL/Services/StudentService.cs
@@ -1,4 +1,7 @@
-﻿using DAL.Repos;
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL.EF.Models;
+using DAL.Repos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +12,36 @@ namespace BLL.Services
 {
     public class StudentService
     {
-        public static dynamic GetAll() {
-             return StudentRepo.GetAll();
+        public static List<StudentDTO> GetAll() {
+             var data= StudentRepo.GetAll();
+             var config = new MapperConfiguration(cfg => {
+                 cfg.CreateMap<Student, StudentDTO>();
+             });
+             var mapper = new Mapper(config);
+             var convrtedd = mapper.Map<List<StudentDTO>>(data);
+            return convrtedd;
         }
-        public static dynamic GetByName() {
-            var data = from s in StudentRepo.GetAll()
+        public static StudentDTO GetByName() {
+            var data = (from s in StudentRepo.GetAll()
                        where s.Name.Equals("Tanvir")
-                       select s;
-            return data;
+                       select s).SingleOrDefault();
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Student, StudentDTO>();
+            });
+            var mapper = new Mapper(config);
+            var convrtedd = mapper.Map<StudentDTO>(data);
+            return convrtedd;
         
+        }
+        public static bool Create(StudentDTO s) {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<StudentDTO, Student>();
+            });
+            var mapper = new Mapper(config);
+            var convrtedd = mapper.Map<Student>(s);
+            var res =StudentRepo.Create(convrtedd);
+            return res;
+
         }
     }
 }
