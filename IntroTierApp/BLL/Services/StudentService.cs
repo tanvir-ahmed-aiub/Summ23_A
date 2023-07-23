06687ebd7bec72d3317a8cdadbacfd5f448f49f0
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2023-07-23 13:34:52 +0600

    Intro Tier

---
+++ IntroTierApp/BLL/Services/StudentService.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class StudentService
+    {
+        public static dynamic GetAll() {
+             return StudentRepo.GetAll();
+        }
+        public static dynamic GetByName() {
+            var data = from s in StudentRepo.GetAll()
+                       where s.Name.Equals("Tanvir")
+                       select s;
+            return data;
+        
+        }
+    }
+}
---
+++ IntroTierApp/DAL/Repos/StudentRepo.cs
@@ -0,0 +1,33 @@
+﻿using DAL.EF;
+using DAL.EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    public class StudentRepo
+    {
+        public static List<Student> GetAll() {
+            var db = new UMSContext();
+            return db.Students.ToList();
+        }
+        public static Student Get(int id) {
+            var db = new UMSContext();
+            return db.Students.Find(id);
+        }
+        public static bool Create(Student s) {
+            var db = new UMSContext();
+            db.Students.Add(s);
+            return db.SaveChanges() > 0;
+        }
+        public static bool Update(Student s) {
+            return false;
+        }
+        public static bool Delete(int id) { 
+            return false;
+        }
+    }
+}
